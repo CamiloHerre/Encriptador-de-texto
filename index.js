cd804061ab004120726563e6282be02e24640e1b
John Herra <camilo2980@gmail.com>
2024-02-22 13:20:10 -0500

    git add .

---
+++ index.js
@@ -0,0 +1,25 @@
+function encriptar (){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let figura = document.getElementById("figura");
+
+    let textoCifrado = texto
+                        .replace(/e/gi, "enter")
+                        .replace(/i/gi, "imes")
+                        .replace(/a/gi, "ai")
+                        .replace(/o/gi, "ober")
+                        .replace(/u/gi, "ufat")
+    
+    if (texto.length != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+        figura.src = "./img/encriptado.png";
+    } else {
+        figura.src = "./img/buscando.png";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algun texto");
+    }
+}
