47747b66ccbfb4eea19b14ee62048e8ac5adf844
John Herra <camilo2980@gmail.com>
2024-02-24 17:23:15 -0500

    Estado final

--- index.js
+++ index.js
@@ -50,3 +50,39 @@ function desencriptar (){
         }                    
 
 }
+
+function copiar (){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    
+    if (texto.length != 0){
+        var contenido = document.querySelector("#texto");
+        contenido.select();
+        document.execCommand("copy");
+        figura.src ="./img/copiar.png";
+        tituloMensaje.textContent = "Texto copiado con éxito";
+    } else {
+        figura.src = "./img/buscando.png";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Heyy!!", "Debes ingresar un texto", "warning");
+        }  
+}
+
+function borrar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+
+    if (texto.length != 0){
+        document.getElementById("texto").value = "";
+        figura.src ="./img/borrar.png";
+        tituloMensaje.textContent = "Texto borrado con éxito";
+    } else {
+        figura.src = "./img/buscando.png";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Heyy!!", "Debes ingresar un texto", "warning");
+        }  
+}
